e3e4bda5072bd423f4d81951aeced79da2ce943e
HelloMizanur <mizanur.rahman0050@gmail.com>
2026-05-21 11:13:31 +0600

    fixed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import dns from "node:dns/promises";
+
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 import "dotenv/config";
@@ -20,38 +21,51 @@ app.use(
     credentials: true,
   }),
 );
+
 app.use(express.json({ limit: "2mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-app.get("/", (_req, res) => res.json({ ok: true, name: "Pet Adoption API" }));
+app.get("/", (_req, res) => {
+  res.json({
+    ok: true,
+    name: "Pet Adoption API",
+  });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/pets", petRoutes);
 app.use("/api/requests", requestRoutes);
 
-app.use((req, res) => res.status(404).json({ message: "Route not found" }));
-app.use((err, _req, res, _next) => {
-  console.error(err);
-  res
-    .status(err.status || 500)
-    .json({ message: err.message || "Server error" });
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
 });
 
-const PORT = process.env.PORT || 5000;
+app.use((err, _req, res, _next) => {
+  console.error(err);
+
+  res.status(err.status || 500).json({
+    message: err.message || "Server error",
+  });
+});
+
 const MONGO_URI = process.env.MONGO_URI;
+
 if (!MONGO_URI) {
-  console.error("MONGO_URI missing in .env");
-  process.exit(1);
+  console.error("MONGO_URI missing");
 }
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`API on http://localhost:${PORT}`));
-  })
-  .catch((e) => {
-    console.error("Mongo connection error:", e.message);
-    process.exit(1);
-  });
+if (mongoose.connection.readyState === 0) {
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err.message);
+    });
+}
+
+export default app;
